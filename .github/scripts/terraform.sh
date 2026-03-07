e4f461838b523984f7d9dd14fbf43a97b7de877e
Ram <ram@Rams-MacBook-Pro.local>
2026-03-07 11:20:21 +0530

    ci cd scripts

--- .github/scripts/terraform.sh
+++ .github/scripts/terraform.sh
@@ -7,6 +7,7 @@ echo "Running $COMMAND"
 
 docker run --rm \
   -v "$(pwd):/workspace" \
+  -w /workspace/infra \
   -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
   -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
   hashicorp/terraform:latest $COMMAND
